174bdf9276ceea7e3ccfc0cf1aca83465cd073c6
fshrmnsfrnd <fshrmnsfrnd@web.de>
2026-01-20 16:41:23 +0100

    Added previous changes from GH

--- preprocessing/build_tags_page.ts
+++ preprocessing/build_tags_page.ts
@@ -1,24 +1,31 @@
 import type { DirNode } from "./build_dir_tree.ts";
 import * as fs from "node:fs";
 
-let tags: Map<String, DirNode[]> = new Map();
+let tags: Map<string, Set<DirNode>> = new Map();
 
 export function processTag(tag: string, file: DirNode) {
-    if(tags.has(tag)){
-        tags.get(tag)?.push(file);
-        return;
+    if(tags.get(tag)){
+        tags.get(tag)?.add(file);
     }else{
-        tags.set(tag, [file]);
+        tags.set(tag, new Set<DirNode>([file]));
     }
+    return;
 }
 
 export function buildTagPage() {
-    const tagsPagePath: string = "./src/pages/tags.html"
-    fs.rmSync(tagsPagePath);
+    const tagsPagePath: string = "./src/pages/tags.astro"
+
+    fs.existsSync(tagsPagePath) ? fs.rmSync(tagsPagePath) : null;
+    fs.writeFileSync(tagsPagePath, `---
+        import Layout from './src/layouts/Layout.astro';
+        ---
+        <Layout>
+        `);
     tags.forEach((nodes, tag) => {
-        fs.appendFileSync(tagsPagePath, `<h1>${tag}<h1>`);
+        fs.appendFileSync(tagsPagePath, `<h1>${tag}</h1>`);
         nodes.forEach((node) => {
-            fs.appendFileSync(tagsPagePath, `<a href="${node.getDirectory()}/${node.getName()}">${node.getName()}</a><br/>`);
+            fs.appendFileSync(tagsPagePath, `<a href="${node.getDirectory()}/${node.getName().replace(/\.[^\/.]+$/, "")}">${node.getName().replace(/\.[^\/.]+$/, "") }</a><br/>`);
         })
     })
+    fs.appendFileSync(tagsPagePath, `</Layout>`);
 }
